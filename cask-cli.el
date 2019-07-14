71aa686016ddbf31bf79b5edb817e8958cb331ac
Miller <john.miller3@honeywell.com>
2019-07-14 09:36:18 -0500

    Try to overcome incovenient encoding prompt.

--- cask-cli.el
+++ cask-cli.el
@@ -27,6 +27,20 @@
 
 ;;; Code:
 
+
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-selection-coding-system
+ (if (eq system-type 'windows-nt)
+     'utf-16-le  ;; https://rufflewind.com/2014-07-20/pasting-unicode-in-emacs-on-windows
+   'utf-8))
+(prefer-coding-system 'utf-8)
+
 (eval-and-compile
   (defconst cask-directory
     (file-name-directory
